98e97324f1580aec2f8f48aecfe3cea84b0e0f31
Axel LAPLACE <axellaplace1709@gmail.com>
2023-11-19 11:13:45 +0100

    email and password valdators added on signup path

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,10 @@ const express = require("express");
 const router = express.Router();
 
 const userCtrl = require("../controllers/user");
+const emailValidator = require("../middleware/emailController")
+const passwordValidator = require("../middleware/passwordControler")
 
-router.post("/signup", userCtrl.signup);
+router.post("/signup", emailValidator, passwordValidator, userCtrl.signup);
 router.post("/login", userCtrl.login);
 
 module.exports = router;
